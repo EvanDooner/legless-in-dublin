9b3ec7d0462838161352d2d04cdddd9129357565
evan <evan@evdev-LAP>
2013-11-18 22:49:22 +0000

    Initial Commit.

---
+++ src/dev/maynooth/mobile/leglessindublin/MainMenu.java
@@ -0,0 +1,29 @@
+package dev.maynooth.mobile.leglessindublin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+	
+	public void searchRatings(View view) {
+		Intent intent = new Intent(this, SearchResultsList.class);
+		startActivity(intent);
+	}
+
+}
